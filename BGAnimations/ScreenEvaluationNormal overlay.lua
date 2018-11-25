941072ca89085afe81330857f871d5b96e1c49f5
Jose_Varela <jo.lu6853@gmail.com>
2018-11-24 21:07:48 -0600

    No fantastics conversion for evaluation

--- BGAnimations/ScreenEvaluationNormal overlay.lua
+++ BGAnimations/ScreenEvaluationNormal overlay.lua
@@ -9,10 +9,17 @@ t[#t+1] = LoadActor( THEME:GetPathG("","Header/Results") )..{
 local JudgmentInfo = {
     Types = { 'W1', 'W2', 'W3', 'W4', 'W5', 'Miss' },
 };
+local yspacing = 32;
+local offsetspritepos = false;
+
+if PREFSMAN:GetPreference("AllowW1") == "AllowW1_Never" then
+    JudgmentInfo.Types = { 'W2', 'W3', 'W4', 'W5', 'Miss' };
+    yspacing = 36;
+    offsetspritepos = true;
+end
 
 local ScoreInfo = Def.ActorFrame{};
 
-local yspacing = 32;
 
 for player in ivalues(PlayerNumber) do
     for index,sco in ipairs(JudgmentInfo.Types) do
@@ -26,6 +33,11 @@ for player in ivalues(PlayerNumber) do
                     local sco = STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetTapNoteScores("TapNoteScore_"..sco)
                     self:settext(("% 4.0f"):format( sco ))
                     :shadowlength(3):zoom(0.8)
+
+                    if not GAMESTATE:IsPlayerEnabled(player) then
+                        self:settext("0000")
+                        self:diffuse(0.5,0.5,0.5,1)
+                    end
                     
                     self:xy(-30,0)
                 end;
@@ -37,6 +49,10 @@ for player in ivalues(PlayerNumber) do
                     local sco = STATSMAN:GetCurStageStats():GetPlayerStageStats(player):GetPercentageOfTaps("TapNoteScore_"..sco)
                     self:settext( FormatPercentScore(sco) )
                     :shadowlength(3):zoom(0.5)
+
+                    if not GAMESTATE:IsPlayerEnabled(player) then
+                        self:settext("0%")
+                    end
                     
                     self:xy(40,0)
                 end;
@@ -49,6 +65,11 @@ for player in ivalues(PlayerNumber) do
         OnCommand=function(self)
             self:settextf( "% 9.0f", GAMESTATE:GetScore(player) )
             :shadowlength(3)
+
+            if not GAMESTATE:IsPlayerEnabled(player) then
+                self:settext("000000000")
+                self:diffuse(0.5,0.5,0.5,1)
+            end
             
             self:xy( _screen.cx+160*GAMESTATE:Side(player) , _screen.cy+160 )
         end;
@@ -59,7 +80,15 @@ for index,sco in ipairs(JudgmentInfo.Types) do
     t[#t+1] = Def.Sprite{
         Texture=THEME:GetPathG("Player judgment/Judgment","label");
         OnCommand=function(self)
-            self:pause():setstate(index-1):zoom(0.8):shadowlength(3)
+            local function spritedelay(val)
+                if val then
+                    if offsetspritepos then
+                        return val
+                    end
+                    return val-1
+                end
+            end
+            self:pause():setstate( spritedelay(index) ):zoom(0.8):shadowlength(3)
             self:xy( _screen.cx, _screen.cy-108+(yspacing*index) )
         end;
     };
